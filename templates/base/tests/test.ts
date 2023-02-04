54c09fad72aa0aedbe6326c0ec62c5ec1c7d6d17
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-02-03 21:12:35 -0500

    add tests to check for interactivity

--- templates/base/tests/test.ts
+++ templates/base/tests/test.ts
@@ -10,3 +10,21 @@ test('index page has expected buttons', async ({ page }) => {
 	expect(await page.textContent('button')).toBe('Call Eel function');
 	expect(await page.textContent('button:nth-of-type(2)')).toBe('Select a file');
 });
+
+test('click on call eel function button', async ({ page }) => {
+	await page.goto('/');
+	await page.waitForLoadState('networkidle');
+	await page.getByRole('button', { name: 'Call Eel function' }).waitFor();
+	await page.getByRole('button', { name: 'Call Eel function' }).click({ clickCount: 2 });
+	expect(await page.textContent('p')).toBe(
+		'Hello World, this function is being called from Python [1]'
+	);
+});
+
+test('click on link to /app', async ({ page }) => {
+	await page.goto('/');
+	await page.waitForLoadState('networkidle');
+	await page.getByRole('link', { name: 'Link to another route' }).waitFor();
+	await page.getByRole('link', { name: 'Link to another route' }).click();
+	await expect(page).toHaveURL('http://localhost:8888/app');
+});
