b161f2d49cc79ed5a2771a638809621aa583b00c
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-02-03 22:09:26 -0500

    add tests for base+tailwind

--- templates/base+tailwind/tests/test.ts
+++ templates/base+tailwind/tests/test.ts
@@ -13,9 +13,12 @@ test('index page has expected buttons', async ({ page }) => {
 
 test('click on call eel function button', async ({ page }) => {
 	await page.goto('/');
+	await page.waitForURL('http://localhost:8888/');
 	await page.waitForLoadState('networkidle');
 	await page.getByRole('button', { name: 'Call Eel function' }).waitFor();
-	await page.getByRole('button', { name: 'Call Eel function' }).click({ clickCount: 2 });
+	await page
+		.getByRole('button', { name: 'Call Eel function' })
+		.click({ clickCount: 2, force: true, delay: 1000 });
 	expect(await page.textContent('p')).toBe(
 		'Hello World, this function is being called from Python [1]'
 	);
@@ -23,8 +26,6 @@ test('click on call eel function button', async ({ page }) => {
 
 test('click on link to /app', async ({ page }) => {
 	await page.goto('/');
-	await page.waitForLoadState('networkidle');
-	await page.getByRole('link', { name: 'Link to another route' }).waitFor();
 	await page.getByRole('link', { name: 'Link to another route' }).click();
 	await expect(page).toHaveURL('http://localhost:8888/app');
 });
